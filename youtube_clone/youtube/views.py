3b5f0e67df5c7f1481a40bd8d2fc48838d66e84d
Ben Fischer <fischerben010@gmail.com>
2021-11-09 17:10:19 -0600

    Added CommentList, ReplyList and Likes and Dislikes to Views

--- youtube_clone/youtube/views.py
+++ youtube_clone/youtube/views.py
@@ -8,3 +8,61 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
+class CommentList(APIView):
+
+    def get(self, request):
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ReplyList(APIView):
+
+    def get(self, request):
+        reply = Reply.objects.all()
+        serializer = ReplySerializer(reply, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentLikes(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        comment.likes += 1
+        comment.save()
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+class CommentDislikes(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        comment.dislikes -= 1
+        comment.save()
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
