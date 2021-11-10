a322e90c365b3ca64f4466b889975208055e6928
Ben Fischer <fischerben010@gmail.com>
2021-11-10 09:20:19 -0600

    Added details class for comments

--- youtube_clone/youtube/views.py
+++ youtube_clone/youtube/views.py
@@ -22,6 +22,32 @@ class CommentList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class CommentDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        comment = self.get_object(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ReplyList(APIView):
 
     def get(self, request):
@@ -62,7 +88,7 @@ class CommentDislikes(APIView):
 
     def get(self, request, pk):
         comment = self.get_object(pk)
-        comment.dislikes -= 1
+        comment.dislikes += 1
         comment.save()
         serializer = CommentSerializer(comment)
         return Response(serializer.data)
